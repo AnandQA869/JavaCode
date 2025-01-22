7c0e2915278f949873d4b1dea090f23fd1cfc121
anand <anandcse435@gmail.com>
2025-01-23 00:34:50 +0530

    MY 4th Code

---
+++ javaPractice/src/javaPracticeTestng/Amz_pom.java
@@ -0,0 +1,5 @@
+package javaPracticeTestng;
+
+public class Amz_pom {
+
+}
---
+++ javaPractice/src/javaPracticeTestng/javaselenium.java
@@ -0,0 +1,11 @@
+package javaPracticeTestng;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class javaselenium {
+public static void main(String[] args) {
+
+	ChromeDriver  driver =new ChromeDriver();
+	
+}
+}
