8bd8874538e5ac1bb3af8adb1bcea8139bcdaf96
anand <anandcse435@gmail.com>
2025-01-23 11:59:31 +0530

    Selenium

---
+++ Selenium/src/SeleniumPractice/OpenLoginURL.java
@@ -0,0 +1,13 @@
+package SeleniumPractice;
+
+public class OpenLoginURL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver= new ChromeDriver();
+		driver.get("");
+
+	}
+
+}
